29ccdc8df3c9782e55a9a81b6c1dccf6daca665b
Vadim Mostovenko <v.mostovenko@spbexchange.ru>
2023-09-24 16:53:41 +0300

    Add yandex metriks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,34 @@ import "../styles/globals.css";
 import Head from "next/head";
 import React from "react";
 import axios from "axios";
+import ym, { YMInitializer } from "react-yandex-metrika";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_DOMAIN + "/api";
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events?.on("routeChangeComplete", (url: string) => {
+    if (typeof window !== undefined) {
+      ym("hit", url);
+    }
+  });
+
   return (
     <>
       <Head>
         <title>MyTop - наш лучший топ</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <meta
           property="og:url"
           content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
         />
         <meta property="og:locale" content="ru_RU" />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
